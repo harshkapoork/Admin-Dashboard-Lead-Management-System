1a15c1d5fd3eeedad7f673224b842376dd0699b2
unknown <harshkapoork>
2025-08-30 16:48:52 +0530

    Your commit message

--- src/components/Pages/EditTaskPage.vue
+++ src/components/Pages/EditTaskPage.vue
@@ -62,7 +62,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="/dist/img/AdminLTELogo.png"   alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src=""   alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Todo App</span>
     </a>
 
@@ -71,7 +71,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Harsh Kapoor</a>
@@ -162,7 +162,6 @@
                 </div>
               </div>
               <div class="card-body">
-                 <h3 class="card-title"><router-link to="/task" ><button class="btn btn-primary"> Task List </button></router-link></h3>
                 <Page/>
           
               </div>
