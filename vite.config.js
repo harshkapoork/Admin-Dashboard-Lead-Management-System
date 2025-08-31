5b84111c550e14e026fce7e33cda51d77df0093b
unknown <harshkapoork>
2025-08-31 05:40:47 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  // base: process.env.NODE_ENV === 'production' ? '/VITE-PROJECT/' : '/',
-
-})
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+  }),
+)
